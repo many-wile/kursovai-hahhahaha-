a00b600b0ff47c6d653092a1aa609a744e8757f7
Evgeniy <soloviev4466@gmail.com>
2026-03-31 14:54:23 +0300

    apply migrations and add SurveysController with GET/POST methods

---
+++ SurveyService.Backend/SurveyService/Survey.Api/Controllers/SurveysController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Survey.Data;
+using Survey.Models;
+
+namespace Survey.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SurveysController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public SurveysController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<SurveyItem>>> GetSurveys()
+    {
+        return await _context.Surveys.Include(s => s.Questions).ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<SurveyItem>> CreateSurvey(SurveyItem item)
+    {
+        _context.Surveys.Add(item);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetSurveys), new { id = item.Id }, item);
+    }
+}
