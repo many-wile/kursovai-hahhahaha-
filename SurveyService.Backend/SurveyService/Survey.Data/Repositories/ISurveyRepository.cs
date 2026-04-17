1216b44da3f704d5f73ab22bff7097ae2d66d6b6
Yuriy Tareev <yuriywinston999@gmail.com>
2026-04-17 16:09:56 +0300

    Паттерн Repository

---
+++ SurveyService.Backend/SurveyService/Survey.Data/Repositories/ISurveyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Survey.Models;
+using Survey.Models.DTO;
+
+namespace Survey.Data.Repositories;
+
+public interface ISurveyRepository
+{
+    Task<PagedResult<SurveyItem>> GetPagedAsync(int page, int pageSize, string query);
+    Task<SurveyItem?> GetByIdAsync(int id);
+    Task<SurveyItem> CreateAsync(SurveyItem item);
+    Task<SurveyItem?> UpdateAsync(int id, SurveyItem item);
+    Task DeleteAsync(int id);
+}
