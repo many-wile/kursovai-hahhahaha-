24f99217b4a25a164b128fde1b6770c96cd1e11b
Evgeniy <soloviev4466@gmail.com>
2026-04-17 14:33:45 +0300

    implement user registration with password hashing

---
+++ SurveyService.Backend/SurveyService/Survey.Api/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Survey.Data;
+using Survey.Models;
+using Survey.Models.DTO;
+
+namespace Survey.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AuthController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserRegisterDto request)
+    {
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+        var user = new User
+        {
+            FullName = request.FullName,
+            Email = request.Email,
+            PasswordHash = passwordHash,
+            Role = "User"
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Ok(user);
+    }
+}
