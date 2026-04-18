9a06f29af08be64262cd4392bf1541caf1d3703a
Yuriy Tareev <yuriywinston999@gmail.com>
2026-04-18 20:32:37 +0300

    Добавлен второй клиент Console Application

---
+++ SurveyService.Backend/SurveyService/Survey.ConsoleClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+using Newtonsoft.Json;
+
+Console.WriteLine("=== Survey Service Console Client ===");
+using var client = new HttpClient();
+
+try
+{
+    var response = await client.GetAsync("https://localhost:7054/api/Surveys?pageSize=5");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var jsonString = await response.Content.ReadAsStringAsync();
+        dynamic data = JsonConvert.DeserializeObject(jsonString);
+
+        Console.WriteLine("\nСписок последних опросов (из API):");
+        foreach (var item in data.items)
+        {
+            Console.WriteLine($"ID: {item.id} | Название: {item.title}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Сервер ответил ошибкой: " + response.StatusCode);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ошибка: убедитесь, что сервер запущен!");
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("\nНажмите любую клавишу для выхода...");
+Console.ReadKey();
